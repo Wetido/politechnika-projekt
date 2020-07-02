66fad4984055bccdb1f654ed410b03a06b29a380
Jakub <wetido@gmail.com>
2020-07-02 17:26:23 +0200

    Added nav bar, displaying feature_imaage and excerpt in main view. Styled.

--- ghostnext/pages/[slug].js
+++ ghostnext/pages/[slug].js
@@ -5,12 +5,39 @@ import { getSinglePost } from './api/posts';
 const PostPage = (props) => {
     // Render post title and content in the page from props
     return (
-      <div>
-        <h1>{props.post.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: props.post.html }} />
-      </div>
-    )
+      <body>
+        <main>
+          <div>
+            <h1>{props.post.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: props.post.html }} />
+          </div>
+        </main>
+
+
+      <style jsx>{`
+
+        
+        body {
+          font-family: 'SF Pro Text', 'SF Pro Icons', 'Helvetica Neue', 'Helvetica',
+            'Arial', sans-serif;
+          padding: 20px 20px 60px;
+          max-width: 680px;
+          margin: 0 auto;
+        }
+
+        .kg-image {
+
+          visibility: hidden;
+        }
+
+      `}</style>
+
+      </body>
+
+      )
   }
+
+
   
   // Pass the page slug over to the "getSinglePost" function
   // In turn passing it to the posts.read() to query the Ghost Content API
@@ -19,4 +46,13 @@ const PostPage = (props) => {
     return { post: post }
   };
   
+
+
+
+
 export default PostPage
+
+
+
+
+
--- ghostnext/pages/author.js
+++ ghostnext/pages/author.js
@@ -1,18 +1,18 @@
-// import { getAuthor } from './api/posts';
+import { getAuthor } from './api/posts';
 
-// const AuthorPage = (props) => {
-//     return (
-//       <div>
-//         <h1>{props.author.name}</h1>
-//         <div dangerouslySetInnerHTML={{ __html: props.author.bio }} />
-//       </div>
-//     )
-//   }
+const AuthorPage = (props) => {
+    return (
+      <div>
+        <h1>{props.author.name}</h1>
+        <div dangerouslySetInnerHTML={{ __html: props.author.bio }} />
+      </div>
+    )
+  }
   
-//   AuthorPage.getInitialProps = async (params) => {
-//     const author = await getAuthor(params.query.slug);
-//     return { author: author }
-//   };
+  AuthorPage.getInitialProps = async (params) => {
+    const author = await getAuthor(params.query.slug);
+    return { author: author }
+  };
   
-//   export default AuthorPage;
+  export default AuthorPage;
   
