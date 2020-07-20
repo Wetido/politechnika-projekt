172e83092711d307a67830aa96d4049f28a561bc
Fileonii <yahhoofj@gmail.com>
2020-07-20 14:56:24 +0200

    super menu does not work yet

--- ghostnext/pages/o-katedrze.js
+++ ghostnext/pages/o-katedrze.js
@@ -4,13 +4,14 @@ import Link from 'next/link';
 import dynamic from 'next/dynamic';
 
 import NavbarThird from '../components/Navbar-third';
-
+import Menu from '../components/about-menu';
 
 const AboutPage = (props) => (
     <body>
   
   <NavbarThird/>
   
+  <Menu/>
       
 
   
